8072bfad709fb1f8be5dd04c5c4067da9a3ff5e0
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-03-01 23:34:42 -0300

    feat: encrypting password with bcrypt.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const db = require("./config/databaseConfig")
-
 const express = require("express")
 const cors = require("cors")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 const app = express()
 
@@ -12,24 +13,30 @@ app.use(cors())
 app.post("/create", (req, res) => {
   const { user, password, email, phone } = req.body
 
-  db.query(
-    "INSERT INTO users (user, password, email, phone) VALUES (?, ?, ?, ?)",
-    [user, password, email, phone],
-    (err) => {
-      if (err) {
-        console.log(err)
-      } else {
-        res.send("user adding.")
-      }
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err)
+    } else {
+      db.query(
+        "INSERT INTO users (user, password, email, phone) VALUES (?, ?, ?, ?)",
+        [user, hash, email, phone],
+        (err) => {
+          if (err) {
+            res.send({ err })
+          } else {
+            res.send("user adding.")
+          }
+        }
+      )
     }
-  )
+  })
 })
 
 // Get all users.
 app.get("/users", (req, res) => {
   db.query("SELECT * FROM users", (err, result) => {
     if (err) {
-      console.log(err)
+      res.send({ err })
     } else {
       res.send(result)
     }
@@ -40,20 +47,22 @@ app.get("/users", (req, res) => {
 app.post("/login", (req, res) => {
   const { user, password } = req.body
 
-  db.query(
-    "SELECT * FROM users WHERE user = ? AND password = ?",
-    [user, password],
-    (err, result) => {
-      if (err) {
-        res.send({ err })
-      }
-      if (result.length > 0) {
-        res.send(result)
-      } else {
-        res.send({ message: "usuario/contraseña incorrecto" })
-      }
+  db.query("SELECT * FROM users WHERE user = ?", user, (err, result) => {
+    if (err) {
+      res.send({ err })
     }
-  )
+    if (result.length > 0) {
+      bcrypt.compare(password, result[0].password, (error, response) => {
+        if (response) {
+          res.send(result)
+        } else {
+          res.send({ message: "contraseña incorrecta" })
+        }
+      })
+    } else {
+      res.send({ message: "el usuario no existe" })
+    }
+  })
 })
 
 // Update user.
@@ -65,7 +74,7 @@ app.put("/update", (req, res) => {
     [user, id],
     (err, result) => {
       if (err) {
-        console.log(err)
+        res.send({ err })
       } else {
         res.send(result)
       }
@@ -78,7 +87,7 @@ app.delete("/delete/:id", (req, res) => {
   const id = req.params.id
   db.query("DELETE FROM users WHERE id = ?", id, (err, result) => {
     if (err) {
-      console.log(err)
+      res.send({ err })
     } else {
       res.send(result)
     }
