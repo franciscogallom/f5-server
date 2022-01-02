5704f98665d1431c0d60c13690006856c0318ce6
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-01-02 18:31:56 -0300

    feat: get bookings of the day and save them all

--- src/api/routes/bookings.ts
+++ src/api/routes/bookings.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from "express"
 import { MongoError, ObjectID } from "mongodb"
+import { MysqlError } from "mysql"
 
 const Booking = require("../../models/Booking")
+const db = require("../../config/mySql")
 
 const router = Router()
 
@@ -16,6 +18,45 @@ interface Bookings {
   startsAt: number
 }
 
+router.get("/today/:user", (req: Request, res: Response) => {
+  const { user } = req.params
+  const today = new Date().toLocaleDateString()
+
+  db.query(
+    `SELECT * FROM bookings WHERE user = '${user}' AND date = '${today}'`,
+    (err: MysqlError, result: any) => {
+      if (err) {
+        res.send({ err })
+      } else {
+        if (result.length > 0) {
+          const hour = result[0].hour
+          const numberOfField = result[0].field_number
+          db.query(
+            `SELECT name, location, price FROM fields WHERE user = '${result[0].field_user}'`,
+            (err: MysqlError, result: any) => {
+              if (err) {
+                res.send({ err })
+              } else {
+                res.send([
+                  {
+                    name: result[0].name,
+                    location: result[0].location,
+                    price: result[0].price,
+                    hour,
+                    numberOfField,
+                  },
+                ])
+              }
+            }
+          )
+        } else {
+          res.send([])
+        }
+      }
+    }
+  )
+})
+
 router.get("/:fieldUsername", (req: Request, res: Response) => {
   const { fieldUsername } = req.params
   Booking.find({ fieldUsername }, (err: MongoError, result: Bookings[]) => {
@@ -25,7 +66,7 @@ router.get("/:fieldUsername", (req: Request, res: Response) => {
 })
 
 router.put("/update", async (req: Request, res: Response) => {
-  const { id, numberOfField, hour } = req.body
+  const { id, numberOfField, hour, user } = req.body
 
   // Example --> numberOfField = 'cancha 1' --> numberOfField.slice(5, 6) = '1'
   const field = `field-${numberOfField.slice(7, 8)}`
@@ -36,9 +77,26 @@ router.put("/update", async (req: Request, res: Response) => {
         res.status(500).send()
       } else {
         newBookings.bookings[field][hour] = false
-        newBookings.markModified("bookings")
-        newBookings.save()
-        res.send("Reserved!")
+        db.query(
+          "INSERT INTO bookings (user, date, field_user, hour, field_number, timestamp) VALUES (?, ?, ?, ?, ?, ?)",
+          [
+            user,
+            new Date().toLocaleDateString(),
+            newBookings.fieldUsername,
+            hour,
+            numberOfField,
+            new Date().toLocaleString(),
+          ],
+          (err: MysqlError) => {
+            if (err) {
+              res.status(500).send()
+            } else {
+              newBookings.markModified("bookings")
+              newBookings.save()
+              res.send("Reserved!")
+            }
+          }
+        )
       }
     })
   } catch (err) {
