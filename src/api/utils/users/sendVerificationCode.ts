2832c59fc5423a101f9dab846be88d8d9fc12dd5
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-03-21 23:41:29 -0300

    improvement: better email verification code messag

--- src/api/utils/users/sendVerificationCode.ts
+++ src/api/utils/users/sendVerificationCode.ts
@@ -8,5 +8,8 @@ export const sendVerificationCode = async (
     from: '"f5" <f5@gmail.com>',
     to: email,
     subject: "🔒 Código de verificación.",
-    html: `<p>Tu código de verificación es <strong>${verificationCode}</strong>.</p>`,
+    html: `<p>Tu código de verificación es <strong>${verificationCode}</strong>.</p>
+          </br>
+          <p>Si no creaste una cuenta en <strong>f5</strong> o no solicitaste un cambio de email en la aplicación, ignora este mensaje.</p>
+        `,
   })
