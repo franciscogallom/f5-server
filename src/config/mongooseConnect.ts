61f455aa3b6c0847c24320bcccbb47577dfeb89d
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-05-16 10:40:41 -0300

    improvement: mongodb por prod and dev

--- src/config/mongooseConnect.ts
+++ src/config/mongooseConnect.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export default mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_DB}:${process.env.MONGO_PASSWORD}@cluster0.dm3po.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+  `mongodb+srv://db-f5:${process.env.MONGO_PASSWORD}@cluster0.dm3po.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
